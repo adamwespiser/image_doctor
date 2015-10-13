e5f79d22a4a8bec75fafff2081e7fa6437c54a0d
Adam Wespiser <adamwespiser@gmail.com>
2015-10-13 02:50:18 -0400

    update __init__

--- image_doctor/__init__.py
+++ image_doctor/__init__.py
@@ -2,83 +2,131 @@
 
 
 
-def test_docstring(param1,param2=None, *args, **kwargs):
-    """This is an example of a module level function.
+import os
+import cv2
+import numpy
+from scipy.ndimage import label
 
-    Function parameters should be documented in the ``Args`` section. The name
-    of each parameter is required. The type and description of each parameter
-    is optional, but should be included if not obvious.
+def segment_on_dt(img):
+    """ takes an openvc image, extracts the central segment, then returns the image
+        with only the central segment, and all other segments colored out(white)
+        args:
+            img: and opencv image object
+        returns:
+            img: and opencv image object of the first segment
+    """
 
-    Parameter types -- if given -- should be specified according to
-    `PEP 484`_, though `PEP 484`_ conformance isn't required or enforced.
+    # code from:
+    # http://stackoverflow.com/questions/11294859/how-to-define-the-markers-for-watershed-in-opencv
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)        
+    _, img_bin = cv2.threshold(img_gray, 0, 255,cv2.THRESH_OTSU)
+    img_bin = cv2.morphologyEx(img_bin, cv2.MORPH_OPEN,numpy.ones((3, 3), dtype=int))
+    border = cv2.dilate(img_bin, None, iterations=5)
+    border = border - cv2.erode(border, None)
 
-    If \*args or \*\*kwargs are accepted,
-    they should be listed as ``*args`` and ``**kwargs``.
+    dt = cv2.distanceTransform(img_bin, 2, 3)
+    dt = ((dt - dt.min()) / (dt.max() - dt.min()) * 255).astype(numpy.uint8)
+    _, dt = cv2.threshold(dt, 180, 255, cv2.THRESH_BINARY)
+    lbl, ncc = label(dt)
+    lbl = lbl * (255/ncc)
+    # Completing the markers now. 
+    lbl[border == 255] = 255 
 
-    The format for a parameter is::
+    lbl = lbl.astype(numpy.int32)
+    cv2.watershed(img, lbl)
 
-        name (type): description
-            The description may span multiple lines. Following
-            lines should be indented. The "(type)" is optional.
+    lbl[lbl == -1] = 0 
+    lbl = lbl.astype(numpy.uint8)
+    result = 255 - lbl 
+    result[result != 255] = 0 
+    result = cv2.dilate(result, None)
+    img[result == 255] = (0, 0, 255)
+    return img 
 
-            Multiple paragraphs are supported in parameter
-            descriptions.
-
+def get_hom_grid_logo(img):
+    """ takes the logo image, determines the homography grid 
     Args:
-        param1 (int): The first parameter.
-        param2 (Optional[str]): The second parameter. Defaults to None.
-            Second line of description should be indented.
-        *args: Variable length argument list.
-        **kwargs: Arbitrary keyword arguments.
+         img: an opencv image object of only the can
+    Return:
+         a collection of pts specifying the grid over the logo"""
+    pass
+    # apply a uniform grid over the logo
 
-    Returns:
-        bool: True if successful, False otherwise.
+def get_hom_grid_can(img):
+    """ takes the segmented can image, determines the homography grid 
+    Args:
+         img: an opencv image object of only the can
+    Return:
+         a collection of pts specifying the grid over the can"""
+    pass
+    # 1) get ellipse formed by the top of the can
+    # 2) get the sides of the can
+    # 3) given the sides of the can, iterate the ellipse in uniform intervals between the 
+    #    sides N times to create a grid of evenly spaced(in 'can' space) points
+def apply_logo_to_can(toimg,fromimg,togird,fromgrid):
+    """ take two images, and their grids and transfers from the toimg onto the fromimg
+        Args:
 
-        The return type is optional and may be specified at the beginning of
-        the ``Returns`` section followed by a colon.
-
-        The ``Returns`` section may span multiple lines and paragraphs.
-        Following lines should be indented to match the first line.
-
-        The ``Returns`` section supports any reStructuredText formatting,
-        including literal blocks::
-
-            {
-                'param1': param1,
-                'param2': param2
-            }
-
-    Raises:
-        AttributeError: The ``Raises`` section is a list of all exceptions
-            that are relevant to the interface.
-        ValueError: If `param2` is equal to `param1`.
-
-
-    .. _PEP 484:
-       https://www.python.org/dev/peps/pep-0484/
-
-    """ 
-    
-    return True
+            toimg: the opencv object to transfer onto
+            fromimg: the opencv object to transfer from
+            togrid: the grid corresponding to the toimg
+            fromgrid: the grid corresponding to the fromimg
+        Return:
+            opencv image of the peicewise affine transform"""
+    pass
+    # to implement, see http://programmingcomputervision.com/downloads/ProgrammingComputerVision_CCdraft.pdf, page 85
 
 
-
+def get_basename(input_file):
+    """ get the basename for a file
+        example '/home/a/b/c.jpg' -> 'c'
+    """
+    return os.path.splitext(os.path.basename(input_file))[0]
 
 
 class template_logos():
-    def __init__(self, templates logos):
-        # register the image files
+    """ the object for initiated the application of logos to template images
+        Args:
+            tempates (list of str): the templates which images are applied to
+            logos    (list of str): the logo images which are applied to the template
+            output_dir (str)       : the output directory 
+            bounding_box (list of (x-min,x-max,y-min,ymax) || None) the bounding box for
+            the template. If list is None, or None is a value in the list, no bounding
+            box is assumed for the template image.
+    """
+    def __init__(self, templates logos, ouputdir,bounding_box=None):
+        self.templates = templates
+        self.logos     = logos
+        self.ouputdir = outputdir
+        self.templates = templates
+    def run_over_imgs(self):
+    """ The function for taking logos and matching them to can typed objects
+        within the template image"""
+        for idx,tmp in enumerate(self.template_logos):
+            box = self.templates[idx[
+            for logo in self.logos:
+                # implement worker queue here
+                output_file = self.outputdir + '/' +  get_basename(tmp) + '_' + get_basename(logo) + '.jpg'
+                status = self.match_logo_to_template(template=tmp, logo=logo, box=box, output_file = output_file)
+                if status is True:
+                    print "PASSED: \n\ttmp: " + tmp + " logo: " + logo + "\toutput: " + output_file
+                else:
+                    print "FAILED: \n\ttmp: " + tmp + " logo: " + logo + "\toutput: " + output_file
 
-    def apply_fn(self):
-        # construct fun call
-        # send to multi-processing
-
-
-
-
-
-
-
-
-
+    def match_logo_to_template(template,logo,box,output_file)
+    """ object method to apply logo to template"""
+    ### apply box to template(not implemented)
+        try:
+            img_tmp = cv2.imread(template)
+            img_logo = cv2.imread(logo)
+        except e:
+            print "failed to open template/log" + '\ttemplate: ' + tmp + '\n\tlogo: ' + logo
+            return None
+        img_tmp_seg = segment_on_dt(tmp)
+        ## get the homography grid on the template
+        img_tmp_grid = get_hom_grid_can(img_tmp_seg)        
+        img_logo_grid  = get_hom_grid_log(img_logo)
+        img_can_logo  = apply_logo_to_can(toimg=img_tmp,fromimg=img_logo,togrid=img_tmp_grid,fromgrid=img_logo_grid)
+        cv2.imwrite(output_file, img_can_logo)
+        return True
 
--- test3.py
+++ test3.py
@@ -1,7 +1,6 @@
 
 
 
-#http://stackoverflow.com/questions/11294859/how-to-define-the-markers-for-watershed-in-opencv
 
 
 import sys
@@ -9,11 +8,17 @@ import cv2
 import numpy
 from scipy.ndimage import label
 
-def segment_on_dt(a, img):
-    border = cv2.dilate(img, None, iterations=5)
+#def segment_on_dt(a, img):
+
+def segment_on_dt(img):
+    #http://stackoverflow.com/questions/11294859/how-to-define-the-markers-for-watershed-in-opencv
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)    
+    _, img_bin = cv2.threshold(img_gray, 0, 255,cv2.THRESH_OTSU)
+    img_bin = cv2.morphologyEx(img_bin, cv2.MORPH_OPEN,numpy.ones((3, 3), dtype=int))
+    border = cv2.dilate(img_bin, None, iterations=5)
     border = border - cv2.erode(border, None)
 
-    dt = cv2.distanceTransform(img, 2, 3)
+    dt = cv2.distanceTransform(img_bin, 2, 3)
     dt = ((dt - dt.min()) / (dt.max() - dt.min()) * 255).astype(numpy.uint8)
     _, dt = cv2.threshold(dt, 180, 255, cv2.THRESH_BINARY)
     lbl, ncc = label(dt)
@@ -22,23 +27,26 @@ def segment_on_dt(a, img):
     lbl[border == 255] = 255
 
     lbl = lbl.astype(numpy.int32)
-    cv2.watershed(a, lbl)
+    cv2.watershed(img, lbl)
 
     lbl[lbl == -1] = 0
     lbl = lbl.astype(numpy.uint8)
-    return 255 - lbl
+    result = 255 - lbl
+    result[result != 255] = 0
+    result = cv2.dilate(result, None)
+    img[result == 255] = (0, 0, 255)
+    return img
 
 
 img = cv2.imread(sys.argv[1])
 
 # Pre-processing.
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)    
-_, img_bin = cv2.threshold(img_gray, 0, 255,
-        cv2.THRESH_OTSU)
-img_bin = cv2.morphologyEx(img_bin, cv2.MORPH_OPEN,
-        numpy.ones((3, 3), dtype=int))
+_, img_bin = cv2.threshold(img_gray, 0, 255,cv2.THRESH_OTSU)
+img_bin = cv2.morphologyEx(img_bin, cv2.MORPH_OPEN,numpy.ones((3, 3), dtype=int))
 
 result = segment_on_dt(img, img_bin)
+
 cv2.imwrite(sys.argv[2], result)
 
 result[result != 255] = 0
